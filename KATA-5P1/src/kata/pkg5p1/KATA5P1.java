d4c0b94d084d120e4d59bae724b6d518890bc2ba
marcos <marcos@marcos-pc>
2018-11-26 10:42:25 +0100

    V2

--- KATA-5P1/src/kata/pkg5p1/KATA5P1.java
+++ KATA-5P1/src/kata/pkg5p1/KATA5P1.java
@@ -8,8 +8,9 @@ import java.sql.Statement;
 public class KATA5P1 {
     public static void main(String[] args) {
         String baseDatos = "KATA5.db";
-        String tabla = "PEOPLE";
-        printDbContent(baseDatos, tabla);
+        String tabla = "EMAIL";
+        //printDbContent(baseDatos, tabla);
+        createNewTable(baseDatos, tabla);
     }
 
     private static void printDbContent(String baseDatos,String tabla) {
@@ -39,4 +40,27 @@ public class KATA5P1 {
             }
         }
     }
+
+    private static void createNewTable(String baseDatos, String tabla) {
+        String url = "jdbc:sqlite:"+ baseDatos;
+        String sql = "CREATE TABLE IF NOT EXISTS " + tabla + " (\n"
+                    + " Id integer PRIMARY KEY AUTOINCREMENT,\n"
+                    + " Mail text NOT NULL);";
+        Connection con = null;
+        try {
+            con = DriverManager.getConnection(url);
+            Statement stmt = con.createStatement();
+            stmt.execute(sql);
+            System.out.println("Tabla creada");
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        } finally {
+            try {
+                if (con != null) con.close();
+            } catch (Exception e) {
+                System.out.println(e.getMessage());
+            }
+        }
+    }
+    
 }
